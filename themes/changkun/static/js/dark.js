f1f29a988730f2c4d3ffd3151d3c43e81ab626ee
marlon <marlon@tfan.net>
2021-04-09 23:05:12 +0800

    always disable dark-mode

--- themes/changkun/static/js/dark.js
+++ themes/changkun/static/js/dark.js
@@ -1,4 +1,6 @@
 var enabled = localStorage.getItem('dark-mode')
+var enabled = 'false'
+
 if (enabled === null) {
   if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     enable(); document.getElementById("dark-mode-checkbox").checked = true;
@@ -6,6 +8,7 @@ if (enabled === null) {
 } else if (enabled === 'true') {
   enable(); document.getElementById("dark-mode-checkbox").checked = true;
 }
+
 function enable()  {
   DarkReader.enable({brightness: 100, contrast: 85, sepia: 10});
   localStorage.setItem('dark-mode', 'true');
